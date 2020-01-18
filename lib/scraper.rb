3f1d7d40c73e379a9ad7b1bacd68e57da76e1cdf
Amanda Rolon <abrolon87@gmail.com>
2020-01-18 17:56:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,3 +5,7 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
 doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each do |course|
+  puts course.text.strip 
+end
